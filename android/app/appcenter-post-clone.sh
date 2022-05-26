54ae3399d471b35d291368a689c453a137e87212
Raji48 <rajeswari3699@gmail.com>
2022-05-26 20:24:12 +0530

    appbuild

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -7,11 +7,8 @@ set -e
 set -x
 
 cd ..
-# choose a different release channel if you want - https://github.com/flutter/flutter/wiki/Flutter-build-release-channels
-# stable - recommended for production
-
-git clone -b stable https://github.com/flutter/flutter.git
-export PATH=`pwd`/flutter/bin:$PATH
+# choose a different release channel if you want - https://github.com/flutter/flutter/wiki/Flutter-build-release-channels# stable - recommended for production
+git clone -b stable https://github.com/flutter/flutter.gitexport PATH=`pwd`/flutter/bin:$PATH
 
 # accepting all licenses
 yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses
@@ -23,7 +20,6 @@ flutter build apk --profile
 
 # if you need build bundle (AAB) in addition to your APK, uncomment line below and last line of this script.
 #flutter build appbundle
-
 # copy the APK where AppCenter will find it
 mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/profile/app-profile.apk $_
 
--- android/app/src/main/kotlin/com/example/sample_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.sample_project
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
